13614e4e67f457f8dee89fed89a651208e8dfa2f
Veds22 <vedant22srivastava@gmail.com>
2026-02-15 14:48:38 +0530

    Initial Commit

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app.database import engine
+from app import models
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "message": "URL Shrotener is running"
+    }
+    
