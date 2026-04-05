c876dd39c6f66436e8cb902c44c1f52edced60b7
Veds22 <vedant22srivastava@gmail.com>
2026-04-05 13:25:21 +0530

    Redis celery optimizations

--- backend/app/core/celery_app.py
+++ backend/app/core/celery_app.py
@@ -6,7 +6,7 @@ REDIS_URL = os.getenv("REDIS_URL")
 celery_app = Celery(
     "worker",
     broker=REDIS_URL,
-    backend=REDIS_URL,
+    backend=None,
     include=[
         "app.tasks.click_tasks",
         "app.tasks.expiry_tasks",
@@ -20,14 +20,14 @@ celery_app.conf.broker_connection_retry_on_startup = True
 celery_app.conf.beat_schedule = {
     "sync-clicks-every-minute": {
         "task": "app.tasks.click_tasks.sync_all_clicks",
-        "schedule": 60.0,  # every 60 seconds
+        "schedule": 300.0,  # every 5 minutes
     }
 }
 
 celery_app.conf.beat_schedule.update({
     "expire-links-every-day": {
         "task": "app.tasks.expiry_tasks.expiry_short_links",
-        "schedule": 600.0,  # every 10 minutes
+        "schedule": 3600.0,  # every 1 hour
     }
 })
 
